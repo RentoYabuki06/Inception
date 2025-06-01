a438668a513015b10d63663aaa21008ea6cd5b9c
RentoYabuki06 <rento.nky.1203@gmail.com>
2025-06-01 16:13:09 +0900

    Update nginx Dockerfile and setup script for SSL certificate paths

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # SSL証明書が存在しない場合は作成
-if [ ! -f /etc/ssl/private/server.crt ]; then
+if [ ! -f /etc/nginx/ssl/server.crt ]; then
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/server.key \
-    -out /etc/ssl/private/server.crt \
+    -keyout /etc/nginx/ssl/server.key \
+    -out /etc/nginx/ssl/server.crt \
     -subj "/C=JP/ST=Tokyo/L=Tokyo/O=42/CN=localhost"
 fi
+
+# 証明書のパスを設定
+ssl_certificate /etc/nginx/ssl/server.crt;
+ssl_certificate_key /etc/nginx/ssl/server.key;
 
 # 起動
 nginx -g "daemon off;"
