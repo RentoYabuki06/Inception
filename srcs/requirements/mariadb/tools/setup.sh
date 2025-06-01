49f884b44bad636185b8bd03494200ae8cb73b79
rento yabuki <rento.nky.1203@gmail.com>
2025-06-01 16:46:06 +0900

    Add setup scripts for MariaDB and WordPress initialization

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# MariaDBの初期化
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+# MariaDB起動
+mysqld_safe --datadir=/var/lib/mysql &
+
+# 起動待機
+sleep 5
+
+# データベースとユーザーの作成
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# フォアグラウンドで実行
+exec mysqld --user=mysql --bind-address=0.0.0.0
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# WordPressのダウンロードと設定
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp core download --allow-root
+    wp config create \
+        --dbname=$WORDPRESS_DB_NAME \
+        --dbuser=$WORDPRESS_DB_USER \
+        --dbpass=$WORDPRESS_DB_PASSWORD \
+        --dbhost=$WORDPRESS_DB_HOST \
+        --allow-root
+    
+    wp core install \
+        --url="https://yabukirento.42.fr" \
+        --title="Inception WordPress" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root
+        
+    wp user create $WP_USER $WP_USER_EMAIL \
+        --user_pass=$WP_USER_PASSWORD \
+        --allow-root
+fi
+
+# PHP-FPM起動
+php-fpm7.4 -F
