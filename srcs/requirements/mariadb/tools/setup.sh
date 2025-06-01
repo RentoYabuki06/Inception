6b7d9d1f5baf9b9aaf445b02440d9497facdf5e6
rento yabuki <rento.nky.1203@gmail.com>
2025-06-01 16:53:43 +0900

    Refactor MariaDB Dockerfile and setup script; remove unused SQL copy command and enhance initialization logic for better reliability.

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-# MariaDBの初期化とセットアップ
-mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+# MariaDBデータディレクトリの初期化
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+fi
 
-# MariaDB起動
+# MariaDBをバックグラウンドで起動
 mysqld_safe --datadir=/var/lib/mysql &
 
-# 起動待機
-sleep 5
+# MariaDBが起動するまで待機
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
 
-# セキュリティ設定とデータベース作成
+# データベースとユーザーの作成
 mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+# セキュリティ設定
 mysql -e "DELETE FROM mysql.user WHERE User='';"
+mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 mysql -e "DROP DATABASE IF EXISTS test;"
+mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
 mysql -e "FLUSH PRIVILEGES;"
 
-# フォアグラウンドで実行
+# バックグラウンドプロセスを停止
+mysqladmin shutdown
+
+# フォアグラウンドでMariaDBを起動
 exec mysqld --user=mysql --bind-address=0.0.0.0
